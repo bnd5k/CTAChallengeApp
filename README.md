47d17965715d9e56af03b9d739b888b72b17950b
Ben Downey <BD@BenDowney.net>
2012-05-05 09:20:52 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This site is a CRM for the CTA Challenge.  The CTA Challenge is to ride every El line and to pass through every El station.  
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CTAChallengeApp::Application.config.secret_token = 'e4a41540bb3550815bf9e14c3ad5c776cc2c48feb378dbcae6a1beb7e2e618c35b5df2a0322db1b2da6229aee76ea0e21d001013490183950749a27adaebde27'
