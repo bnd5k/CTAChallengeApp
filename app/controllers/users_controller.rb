22719bd7b518911ec8302296a4e4bff7f8c97449
Ben Downey <BD@BenDowney.net>
2012-05-05 09:59:05 -0500

    users CRUD setup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    if save?
+      flash[:success] = "Welcome!"
+      redirect_to root_url
+    else
+      render 'new'
+      flash[:error] = "Please try again."
+    end
+  end
+
+  def edit 
+    @user = User.find_by_id(params[:id])
+  end
+
+  def update
+    @user = User.find_by_id(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      redirect_to user_url
+    else
+      render 'edit'
+      flash[:error] = "Please try again."
+    end
+  end
+
+  def delete
+   User.find_by_id(params[:id]).destroy
+   flash[:success] = "User has been deleted"
+   redirect_to root_url
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,19 @@
 CTAChallengeApp::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+  root :to => 'users#index'
+  
+  get '/users', :controller => :users, :action => :index, :as => :users
+  get '/user/:id', :controller => :users, :action => :show, :as => :user
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
+  get '/users/new', :controller => :users, :action => :new, :as => :new_user
+  post '/users/create', :controller => :users, :action => :create
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  get '/user/:id/edit', :controller => :users, :action => :edit, :as => :edit_user
+  put 'user/:id/update', :controller => :users, :action => :update
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  delete 'users/:id', :controller => :users, :action => :destroy
 
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
 
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
 
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
